340b1b1144cc676edbc82754f5600eb016133819
Tran <tranphan150202@gmail.com>
2024-11-18 15:48:24 +0700

    first

--- components/SaleBanner.tsx
+++ components/SaleBanner.tsx
@@ -9,7 +9,7 @@ async function SaleBanner() {
   }
 
   return (
-    <div className="sale-banner bg-gradient-to-r from-red-500 to-black text-white px-6 py-10 mx-4 mt-2 rounded-lg shadow-lg">
+    <div className="sale-banner bg-gradient-to-r from-blue-500 to-black text-white px-6 py-10 mx-4 mt-2 rounded-lg shadow-lg">
       <div className="container mx-auto flex item-center justify-between">
         <div className="flex-1">
           <h2 className="text-3xl sm:text-5xl font-extrabold text-left mb-4">
